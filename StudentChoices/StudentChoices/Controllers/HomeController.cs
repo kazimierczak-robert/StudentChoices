b2ebfceb974d7182d49f6d6842d87ac1929cf789
kazimierczak-robert <kazimierczak-robert@tlen.pl>
2017-05-26 19:21:53 +0200

    Wyświetlanie hiperłączy do plików powiązanych z przedmiotami

--- StudentChoices/StudentChoices/Controllers/HomeController.cs
+++ StudentChoices/StudentChoices/Controllers/HomeController.cs
@@ -184,7 +184,7 @@ namespace StudentChoices.Controllers
                                                     var files = String.Empty;
                                                     foreach (var file in db.Files.Where(x => x.ElectiveSubjectAndSpecialityID == Sub.ElectiveSubjectAndSpecialityID))
                                                     {
-                                                        files += file.Path + "\n";
+                                                        files += file.Filename + " " + file.Path + " ";
                                                     }
                                                     SubInfo.Add(files);
                                                     optionsOneCategoryList.Add(SubInfo);
